b2247c472d76ef96783e9a0d31c415180407875d
Gustavo Rezende <nsigustavo@gmail.com>
2010-07-20 04:25:02 -0300

    add script para gerar html

---
+++ generate.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [[ -d ludibrio ]]; then
+    cd ludibrio
+    git pull origin ludibrio3.0
+    cd ..
+else
+    git clone git@github.com:nsigustavo/ludibrio.git
+fi
+
+
+make clean
+make html
+
+cp -rf build/html/* ludibrio/docs
+
+
+
+
+if [ -d google_appengine]; then else
+    wget http://googleappengine.googlecode.com/files/google_appengine_1.3.5.zip
+    unzip  google_appengine_1.3.5.zip
+    rm google_appengine_1.3.5.zip
+fi
+
+
+cp -rf build/html/* ludibrio/docs
